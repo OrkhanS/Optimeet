bb659b6012d1ad134705d5d907b03ae5ecd9f5b2
Orkhan Salahov <orkhan7887@gmail.com>
2020-04-16 16:16:49 +0300

    settings changed

--- optimeet/settings.py
+++ optimeet/settings.py
@@ -111,23 +111,23 @@ ASGI_APPLICATION = 'optimeet.routing.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'briddgyc_optimeet',
-#         'USER' : 'briddgyc_rustam',
-#         'PASSWORD' : 'Decemption@111',
-#         'HOST' : 'localhost',
-#         'PORT' : '5432'
-#     }
-#     }   
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'briddgyc_optimeet',
+        'USER' : 'briddgyc_rustam',
+        'PASSWORD' : 'Decemption@111',
+        'HOST' : 'localhost',
+        'PORT' : '5432'
     }
-}
+    }   
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 AUTH_USER_MODEL = 'APP.User'
